bc739cf0983ce1d849b7cc57e84ce953ebebca87
Annerez <98201562+Annerez@users.noreply.github.com>
2022-04-09 21:10:51 +0700

    Create Notify.py

---
+++ Notify.py
@@ -0,0 +1,10 @@
+#pip install songline
+from songline import Sendline
+
+token = 'Place your line notify token here'
+
+messenger = Sendline(token)
+
+#messenger.sendtext('')
+#messenger.sticker(1,1)
+#messenger.sendimage('')
